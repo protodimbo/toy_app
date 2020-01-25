09f14ec0e5e08619a5648bf1e57fe69222e402ca
protodimbo <dimanvasya@yandex.ru>
2020-01-25 13:58:21 +0300

    concatenate 2 bd

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,4 @@
 class Micropost < ApplicationRecord
+    belongs_to :user #микросообщения принадлежат юзеру
+    validates :content, length: {minimum:1, maximum:140} #проверка длины микросообщения
 end
